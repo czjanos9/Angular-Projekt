faca4faa5f792accf0b7a37c681e1b26a6fc6dab
czjanos9 <cz.janos9@gmail.com>
2021-05-13 02:53:26 +0200

    Adding record AS NEW

--- src/app/tank-details/tank-details.component.ts
+++ src/app/tank-details/tank-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {Tanks} from '../list/list.component';
 
@@ -13,6 +13,7 @@ export interface Tank {
   nation: string;
   gun_name: string;
   armor: string;
+  small_icon: string;
 }
 
 @Component({
@@ -33,40 +34,34 @@ export class TankDetailsComponent implements OnInit {
     description: 'data[description]',
     armor: '0mm/0mm/0mm',
     gun_name: 'data[default_profile][gun][name]',
+    small_icon: ''
   };
 
   allTank;
   displayedColumns: string[] = ['nation', 'tier', 'name', 'big_icon', 'type', 'description', 'armor', 'gun_name'];
   dataSource;
 
-
   constructor(
     private activatedRoute: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(param => {
       this.id = param['id'];
     });
-    //this.doRequest(`https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=7d8440b17a9bc5cbd0f8be1dc99a4fb6&tank_id=${this.id}`, this.id);
     this.allTank = JSON.parse(localStorage.getItem('AllVehicles'));
     for(let i in this.allTank){
       if(i === this.id) {
         this.currTank = this.createDataSource(this.allTank[this.id]);
-      }
-      else {
-        //this.doRequest(`https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=7d8440b17a9bc5cbd0f8be1dc99a4fb6&tank_id=${this.id}`, this.id);
-
+        localStorage.setItem('CurrentVehicle', JSON.stringify(this.currTank));
       }
     }
   }
 
-  doRequest(url, id): void {
+  /*doRequest(url, id): void {
     this.http.get<any>(url).subscribe(data => {
-      /*this.currTank.shift();
-      this.currTank.push(this.createDataSource(data.data[id]));
-      this.dataSource = this.currTank;*/
       this.currTank = this.createDataSource(data.data[id]);
       localStorage.setItem('CurrentVehicle', JSON.stringify(this.currTank));
       let localData: JSON = JSON.parse(localStorage.getItem('AllVehicles'));
@@ -80,12 +75,7 @@ export class TankDetailsComponent implements OnInit {
 
 
     });
-  }
-
-  getCurrentTank()
-  {
-
-  }
+  }*/
 
   createDataSource(data): Tank {
     let tank = {
@@ -119,7 +109,53 @@ export class TankDetailsComponent implements OnInit {
     return;
   }
 
+  deleteCurrentVehicle(): void {
+    localStorage.setItem('CurrentVehicle', JSON.stringify(this.currTank));
+
+    let localData: JSON = JSON.parse(localStorage.getItem('AllVehicles'));
+    delete localData[this.currTank['tank_id']];
+
+    localStorage.setItem('AllVehicles', JSON.stringify(localData));
+    alert('Sikeres törlés!');
+    this.router.navigate(['list']);
+
+  }
+
+  createVehicle(): void{
+    localStorage.setItem('CurrentVehicle', JSON.stringify(this.currTank));
+    let prevId = this.id;
+
+    this.currTank['tank_id'] = Math.round(Math.random() * (2147 - 1) + 1);
+    this.allTank = JSON.parse(localStorage.getItem('AllVehicles'));
+    this.id = this.currTank['tank_id'].toString();
+
+    for(let i in this.allTank){
+      if(i === this.id) {
+        alert('Van ilyen azonosító!');
+        return;
+      }
+      let localData: JSON = JSON.parse(localStorage.getItem('AllVehicles'));
+      localData[this.id] = {
+        tank_id: this.id,
+        name: this.currTank['name'],
+        nation: this.currTank['nation'],
+        type: this.currTank['type'],
+        tier: this.currTank['tier'],
+        description: this.currTank['description'],
+        images: {small_icon: this.currTank['small_icon'],
+                 big_icon: this.currTank['big_icon']},
+        default_profile: {gun: {name: this.currTank['gun_name']},
+                          armor: {hull: {front: localData[prevId]['default_profile']['armor']['hull']['front'],
+                                        sides: localData[prevId]['default_profile']['armor']['hull']['sides'],
+                                        rear: localData[prevId]['default_profile']['armor']['hull']['rear']}}}
+      };
+      localStorage.setItem('AllVehicles', JSON.stringify(localData));
+      alert('Sikeres hozzáadás!');
+      return;
+    }
+  }
+
 }
 
 
-/*https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=7d8440b17a9bc5cbd0f8be1dc99a4fb6&tank_id=19217*/
+/*this.doRequest(`https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=7d8440b17a9bc5cbd0f8be1dc99a4fb6&tank_id=${this.id}`, this.id);*/
